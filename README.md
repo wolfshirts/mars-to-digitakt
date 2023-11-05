e697a1fc2e7f4d6469918796d839f0381bf9ec4e
ross <ross.russell@protonmail.com>
2023-11-05 13:38:35 -0800

    parse out non c files, keep drums.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mars-to-digitakt
+Just got a digitakt, I'm using samples from mars packs. The packs take a lot of space.
+This tool takes an extracted zip, parses through all the directories and builds a zip
+which contains all the c samples, and tracks without a root note.
+
+The directory should be an unzipped samples from mars.
+
+To use python3 mars_digitakt.py /your/directory/here
---
+++ mars_digitakt.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import re
+import zipfile
+
+def argsParse(args):
+    if len(args) == 1:
+        print("enter path to folder")
+        sys.exit()
+    print(args[1])
+    return args[1]
+
+def is_wav(filename):
+    f = filename.split(".")
+    if f[-1].lower() == "wav":
+        return True
+    return False
+
+def is_c_or_non_pitched(filename):
+    f = filename.split(" ")[-1].split(".")[0]
+    pitched = re.search("[ABCDEFGabcdefg]#?-?[0-9]", f)
+    if "c" in f.lower() and "#" not in f.lower():
+        return True
+    if not pitched:
+        return True
+    return False
+
+    
+def get_path_list(path):
+    to_zip = []
+    res = os.walk(path)
+    for root, dirs, files in res:
+        if len(files) > 0:
+            for i in files:
+                if is_wav(i) and is_c_or_non_pitched(i):
+                    to_zip.append(os.path.join(root, i))
+    return to_zip
+
+def create_zip(files):
+    with zipfile.ZipFile(sys.argv[1].split("/")[-1], 'w') as zipped:
+        for f in files:
+            zipped.write(f, compress_type=zipfile.ZIP_DEFLATED)
+
+path = argsParse(sys.argv)
+if os.path.isdir(path):
+    res = get_path_list(path)
+    create_zip(res)
+else:
+    print("not a directory")
+    sys.exit()
